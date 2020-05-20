7b3e3eee8a72bde567acc38c0ad515fa04367e5e
meghnavarma0 <meghnavarma0@gmail.com>
2020-05-20 18:47:28 +0530

    dynamically accessing ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+	state = {
+		ingredients: {
+			salad: 0,
+			bacon: 0,
+			cheese: 0,
+			meat: 0
+		}
+	};
 	render() {
 		return (
 			<Auxiliary>
-				<div>Burger</div>
+				<Burger ingredients={this.state.ingredients} />
 				<div>Build Cintrols</div>
 			</Auxiliary>
 		);
